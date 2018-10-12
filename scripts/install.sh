3f66ce614d07c759d84cbd90a5207237953abe67
Dan Reeves <hey@danreev.es>
2018-10-11 21:07:58 -0700

    try installing from cargo

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,18 +30,7 @@ main() {
       ;;
   esac
 
-  # This fetches latest stable release
-  local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-    | cut -d/ -f3 \
-    | grep -E '^v[0.1.0-9.]+$' \
-    | $sort --version-sort \
-    | tail -n1)
-  curl -LSfs https://japaric.github.io/trust/install.sh | \
-    sh -s -- \
-    --force \
-    --git japaric/cross \
-    --tag $tag \
-    --target $target
+  cargo install cross
 
   # Install test dependencies
   rustup component add rustfmt-preview
